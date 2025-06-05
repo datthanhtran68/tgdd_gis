93d0aca25991bc86c4f1e60349d4c26d1c746365
datthanhtran68 <118030101+datthanhtran68@users.noreply.github.com>
2025-06-05 22:30:07 +0700

    Update script.js

--- static/js/script.js
+++ static/js/script.js
@@ -507,7 +507,7 @@ function login(event) {
     const username = document.getElementById('username').value;
     const password = document.getElementById('password').value;
 
-    fetch('http://localhost:5000/api/login', {
+    fetch('https://tgdd-gis.onrender.com//api/login', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ username, password })
