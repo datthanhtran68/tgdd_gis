beeab3ee7746deae909f0286ad4f3aa05d12cf24
datthanhtran68 <118030101+datthanhtran68@users.noreply.github.com>
2025-06-11 01:45:31 +0700

    Update script.js

--- static/js/script.js
+++ static/js/script.js
@@ -587,7 +587,7 @@ function changePassword(event) {
     const oldPassword = document.getElementById('oldPassword').value;
     const newPassword = document.getElementById('newPassword').value;
 
-    fetch('https://tgdd-gis.onrender.com//api/change-password', {
+    fetch('https://tgdd-gis.onrender.com/api/change-password', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ username, oldPassword, newPassword })
