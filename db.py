433e26db23a2caac818ce251061468f6b5a9b58a
datthanhtran68 <118030101+datthanhtran68@users.noreply.github.com>
2025-06-11 00:59:03 +0700

    Update db.py

--- db.py
+++ db.py
@@ -1,14 +1,7 @@
-# db.py
 import psycopg2
-import logging
+import os
 
 def get_db_connection():
-    conn = psycopg2.connect(
-        dbname="tgdd_gis",
-        user="postgres",
-        password="1",
-        host="localhost",
-        port="5432"
-    )
-    logging.debug("Kết nối database thành công")
+    conn = psycopg2.connect(os.getenv("DATABASE_URL"))
     return conn
+
