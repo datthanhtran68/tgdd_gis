d50dc06f96c4d3b1af9f83c088a7e1aba350f3f4
datthanhtran68 <118030101+datthanhtran68@users.noreply.github.com>
2025-06-11 01:14:30 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,3 @@
-# config.py
 import os
 
-# Lấy cấu hình từ biến môi trường DATABASE_URL
-DATABASE_URL = os.environ.get('DATABASE_URL', 'postgresql://postgres:1@localhost:5432/tgdd_gis')
+DATABASE_URL = os.environ.get("DATABASE_URL")
